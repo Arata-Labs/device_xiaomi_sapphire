lycodump/android_device_xiaomi_sapphire

sapphire: migrate TARGET_INIT_VENDOR_LIB to soong_config

device.mk
@@ -24,6 +24,9 @@ TARGET_HAS_UDFPS := true
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_alioth)
+
 # Overlays
 PRODUCT_PACKAGES += \
     ApertureResSapphire \

Arata-Labs/device_xiaomi_sapphire

device.mk
@@ -24,6 +24,9 @@ TARGET_HAS_UDFPS := true
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_sapphire)
+
 # Overlays
 PRODUCT_PACKAGES += \
     ApertureResSapphire \