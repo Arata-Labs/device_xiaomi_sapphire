97e2642d48141871ee9c6276173711aa1c5140e5
Angelpro09_Dev <aloplop090708@gmail.com>
2025-11-28 08:26:04 +0000

    sapphire: add inherits for miuicamera-sapphire

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,9 @@ include device/xiaomi/sm6225-common/BoardConfigCommon.mk
 # Inherit from the proprietary version
 include vendor/xiaomi/sapphire/BoardConfigVendor.mk
 
+# MiuiCamera
+include device/xiaomi/miuicamera-sapphire/BoardConfig.mk
+
 DEVICE_PATH := device/xiaomi/sapphire
 KERNEL_PATH := $(DEVICE_PATH)-kernel
 
